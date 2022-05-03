6d2236a86784774284c5cb7380d8748a4ff3daa8
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-03 14:09:13 -0300

    first part of the second test of the first task

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,11 @@
 function stringhLength(string) {
-  return string.length;
+  if (string.length >= 1 && string.length <= 10) {
+    return string.length;
+  } else if(string.length < 1) {
+    throw new Error('there is not a word');
+  } else {
+    throw new Error('the word has more than 10 characters');
+  }
 }
 
 module.exports = stringhLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,15 @@
 const stringhLength = require('./stringLength');
 
-test('hola to be 4', () => {
-  expect(stringhLength('hola')).toEqual(4);
+test('hello to be 5', () => {
+  expect(stringhLength('hello')).toEqual(5);
 });
+
+test('A to be 1', () => {
+  expect(stringhLength('A')).toEqual(1);
+});
+
+test('Microverse to be 10', () => {
+  expect(stringhLength('Microverse')).toEqual(10);
+});
+
+// test('no word to throw an error', )
